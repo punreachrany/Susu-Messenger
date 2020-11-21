e6ab1c67af77245a8983b8c1ea0efe265541974e
Punreach Rany <punreachrany@Punreachs-MacBook-Pro.local>
2020-11-22 00:00:08 +0900

    Chatroom ui

--- lib/screens/chat/chatroom.dart
+++ lib/screens/chat/chatroom.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:Susu_Messenger/themes/colors.dart';
 import 'package:flutter/material.dart';
 
 class Chatroom extends StatefulWidget {
@@ -151,9 +152,14 @@ class _ChatroomState extends State<Chatroom> {
         "user": "Punrong Rany",
         "picture": Icons.person,
         "message":
-            "Ignoring header X-Firebase-Locale because its value was null"
+            "Ignoring header X-Firebase-Locale because its value was null. I cannot believe it at all. He is awesome."
       },
-      {"user": "Punreach Rany", "picture": Icons.person, "message": "Vola"},
+      {
+        "user": "Punreach Rany",
+        "picture": Icons.person,
+        "message":
+            "Ignoring header X-Firebase-Locale because its value was null. I cannot believe it at all. He is awesome."
+      },
     ];
 
     // if (mounted) {
@@ -172,6 +178,83 @@ class _ChatroomState extends State<Chatroom> {
         (_) => {controller.jumpTo(controller.position.maxScrollExtent)});
   }
 
+  Widget otherPeopleMessage({String username, String messages}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: CircleAvatar(child: Icon(Icons.person)),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(username),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[700],
+                      borderRadius: BorderRadius.circular(10)),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    messages,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userMessage({String username, String messages}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 8,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  color: primaryColor, borderRadius: BorderRadius.circular(10)),
+              alignment: Alignment.centerLeft,
+              child: Text(messages),
+            ),
+          ),
+          // Expanded(
+          //   flex: 1,
+          //   child: Icon(Icons.person),
+          // ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // controller.animateTo(controller.position.maxScrollExtent,
@@ -182,14 +265,40 @@ class _ChatroomState extends State<Chatroom> {
         title: Text("Hanyang University"),
       ),
       bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         width: double.infinity,
-        // height: 40,
+        color: Colors.grey[200],
+        // decoration: BoxDecoration(
+        //     border: Border(top: BorderSide(color: primaryColor, width: 2.0))),
+        // height: 40
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Expanded(flex: 8, child: TextFormField()),
+            Expanded(flex: 1, child: Icon(Icons.add_box)),
+            Expanded(
+                flex: 8,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[300],
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextFormField(
+                    maxLines: 1,
+                    decoration: new InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.only(
+                            left: 15, bottom: 11, top: 11, right: 15),
+                        hintText: 'Type your message'),
+                  ),
+                )),
             Expanded(flex: 1, child: Icon(Icons.send))
           ],
         ),
@@ -202,25 +311,33 @@ class _ChatroomState extends State<Chatroom> {
           controller: controller,
           itemBuilder: (context, index) {
             if (chat[index]["user"] == "Punreach Rany") {
-              return ListTile(
-                // leading: Icon(chat[index]["picture"]),
-                title: Text(chat[index]["user"]),
-                subtitle: Text(chat[index]["message"]),
-                trailing: Icon(chat[index]["picture"]),
-                onTap: () {
-                  print(chat[index]["user"]);
-                },
+              return userMessage(
+                messages: chat[index]["message"],
+                username: chat[index]["user"],
               );
+              // return ListTile(
+              //   // leading: Icon(chat[index]["picture"]),
+              //   title: Text(chat[index]["user"]),
+              //   subtitle: Text(chat[index]["message"]),
+              //   trailing: Icon(chat[index]["picture"]),
+              //   onTap: () {
+              //     print(chat[index]["user"]);
+              //   },
+              // );
             } else {
-              return ListTile(
-                leading: Icon(chat[index]["picture"]),
-                title: Text(chat[index]["user"]),
-                subtitle: Text(chat[index]["message"]),
-                // trailing: Icon(chat[index]["picture"]),
-                onTap: () {
-                  print(chat[index]["user"]);
-                },
+              return otherPeopleMessage(
+                messages: chat[index]["message"],
+                username: chat[index]["user"],
               );
+              // return ListTile(
+              //   leading: Icon(chat[index]["picture"]),
+              //   title: Text(chat[index]["user"]),
+              //   subtitle: Text(chat[index]["message"]),
+              //   // trailing: Icon(chat[index]["picture"]),
+              //   onTap: () {
+              //     print(chat[index]["user"]);
+              //   },
+              // );
             }
           },
         ),
